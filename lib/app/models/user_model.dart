af47ffdfabb77c35c001f1c0943658277f3a9cef
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-08-04 23:29:03 -0300

    criação das propriedades e métodos do UserModel

--- lib/app/models/user_model.dart
+++ lib/app/models/user_model.dart
@@ -1,4 +1,54 @@
+import 'dart:convert';
+
 class UserModel{
   int? id;
   String? name;
+  String? email;
+  String? password;
+  DateTime? createdAt;
+  String? token;
+
+  UserModel({
+    this.id,
+    this.name,
+    this.email,
+    this.password,
+    this.createdAt,
+    this.token
+  });
+
+  Map<String, dynamic> toMap(){
+    return {
+      "id": id,
+      "name": name,
+      "email": email,
+      "password": password,
+      "created_at": createdAt?.millisecondsSinceEpoch,
+      "token": token,
+    };
+  }
+
+  factory UserModel.fromMap(Map<String, dynamic> map){
+    return UserModel(
+      id: map["id"]?.toInt(),
+      name: map["name"],
+      email: map["email"],
+      password: map["password"],
+      createdAt: map["created_at"] != null ? DateTime.fromMillisecondsSinceEpoch(map["created_at"]) : null,
+      token: map["token"]
+    );
+  }
+
+  static List<UserModel> fromList(list){
+    return List<UserModel>.from(list.map((x) => UserModel.fromMap(x)));
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserModel.fromJson(String source) => UserModel.fromMap(json.decode(source));
+
+  @override
+  String toString() {
+    return 'UserModel(id: $id, name: $name, email: $email, password: $password, createdAt: $createdAt, token: $token)';
+  }
 }
