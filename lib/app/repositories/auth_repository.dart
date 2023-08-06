68f0f2bcb1e511347f88bda66f4f12106f011d65
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-08-06 00:29:38 -0300

    criação do arquivo e suas propriedades de api_result

--- lib/app/repositories/auth_repository.dart
+++ lib/app/repositories/auth_repository.dart
@@ -1,3 +1,12 @@
+import 'package:myfinance/app/core/services/http_manager.dart';
+import 'package:myfinance/app/core/utils/app_utils.dart';
+
 class AuthRepository{
-  
+  final HttpManager httpManager;
+  final AppUtils appUtils;
+
+  AuthRepository({
+    required this.httpManager,
+    required this.appUtils
+  });
 }
