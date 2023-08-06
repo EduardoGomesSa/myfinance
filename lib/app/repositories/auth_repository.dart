10772a703b29617eb73c816788e976a2b0e1f1ae
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-08-06 00:48:17 -0300

    criação do método de login no authrepository

--- lib/app/repositories/auth_repository.dart
+++ lib/app/repositories/auth_repository.dart
@@ -1,5 +1,9 @@
 import 'package:myfinance/app/core/services/http_manager.dart';
+import 'package:myfinance/app/core/utils/api_result.dart';
 import 'package:myfinance/app/core/utils/app_utils.dart';
+import 'package:myfinance/app/models/user_model.dart';
+
+import '../core/utils/Urls.dart';
 
 class AuthRepository{
   final HttpManager httpManager;
@@ -10,5 +14,26 @@ class AuthRepository{
     required this.appUtils
   });
 
-  
+  Future<ApiResult<UserModel>> signIn({required String email, required String password}) async {
+    const String endpoint = "${Url.base}/login";
+
+    final response = await httpManager.request(
+      url: endpoint, 
+      method: HttpMethods.post,
+      body: {
+        "email": email,
+        "password": password,
+      }
+    );
+
+    if(response["data"] != null){
+      UserModel user = UserModel.fromMap(response['data']);
+      appUtils.saveLocalData(key: "user-token", data: user.token!);
+
+      return ApiResult<UserModel>(data: user);
+    }else{
+      String message = response['error'] ?? "Não foi possivel fazer login. Tente novamente";
+      return ApiResult<UserModel>(message: message, isError: true);
+    }
+  }
 }
