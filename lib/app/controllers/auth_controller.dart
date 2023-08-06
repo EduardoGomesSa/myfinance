bc05ac5ee88dd962841b2f8d25342217f2ac93b6
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-08-05 23:26:11 -0300

    criação do arquivo de authcontroller

---
+++ lib/app/controllers/auth_controller.dart
@@ -0,0 +1,18 @@
+import 'package:get/get.dart';
+import 'package:myfinance/app/models/user_model.dart';
+
+class AuthController extends GetxController{
+  RxBool isLoading = false.obs;
+  UserModel user = UserModel();
+
+  @override
+  void onInit(){
+    super.onInit();
+
+
+  }
+
+  Future signUp() async {
+
+  }
+}
