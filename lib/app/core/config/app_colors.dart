966a3733ead22fc9bc1ed1ec95532083e8aac99c
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-08-06 23:21:47 -0300

    criação do arquivo app_colors

---
+++ lib/app/core/config/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+abstract class AppColors {
+  static const Color primary = Color(0XFF725DDD);
+  static const Color background = Color(0XFF121214);
+  static const Color backgroundComponent = Color(0XFF202024);
+  static const Color secundary = Color(0XFFF2E205);
+  static const Color primaryText = Color(0XFFECEBEB);
+  static const Color secundaryText = Color(0XFF696667);
+}
