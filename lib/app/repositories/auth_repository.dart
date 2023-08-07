c5ca5fd0be966e97f01e22a01a5bb1b708204672
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-08-06 23:06:42 -0300

    criação dos metodos restantes de auth_repository

--- lib/app/repositories/auth_repository.dart
+++ lib/app/repositories/auth_repository.dart
@@ -32,8 +32,73 @@ class AuthRepository{
 
       return ApiResult<UserModel>(data: user);
     }else{
-      String message = response['error'] ?? "Não foi possivel fazer login. Tente novamente";
+      String message = response['error'] ?? "Não foi possível fazer login. Tente novamente!";
       return ApiResult<UserModel>(message: message, isError: true);
+    }
+  }
+
+  Future<ApiResult<UserModel>> signUp(UserModel user) async {
+    const String endpoint = "${Url.base}/register";
+
+    Map<String, dynamic> body = user.toMap();
+    body['password_confirmation'] = user.password;
+
+    final response = await httpManager.request(
+      url: endpoint, 
+      method: HttpMethods.post,
+      body: body
+    );
+
+    if(response['data'] != null){
+      UserModel user = UserModel.fromMap(response['data']);
+
+      return ApiResult<UserModel>(data: user);
+    }else {
+      String message = response['error'] ?? "Não foi possível fazer o cadastro. Tente novamente!";
+
+      return ApiResult<UserModel>(message: message, isError: true);
+    }
+  }
+
+  Future<ApiResult<UserModel>> validateToken(String token) async {
+    const String endpoint = "${Url.base}/validate-token";
+
+    final response = await httpManager.request(
+      url: endpoint, 
+      method: HttpMethods.post,
+      headers: {
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if(response['data'] != null){
+      UserModel user = UserModel.fromMap(response['data']);
+
+      appUtils.saveLocalData(key: "user-token", data: user.token!);
+
+      return ApiResult<UserModel>(data: user);
+    } else {
+      String message = response['error'] ?? "Realize o login novamente";
+
+      return ApiResult<UserModel>(message: message, isError: true);
+    }
+  }
+
+  Future<String?> signOut ({required String token}) async {
+    const String endpoint = "${Url.base}/logout";
+
+    final response = await httpManager.request(
+      url: endpoint, 
+      method: HttpMethods.post,
+      headers: {
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if(response['message'] != null){
+      return response['message'];
+    } else {
+      return null;
     }
   }
 }
