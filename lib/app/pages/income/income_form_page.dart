7b695a35616fbc1a71e4254fc78a4eb6814f4bc8
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-08-22 23:44:19 -0300

    crição do campo de ganho

--- lib/app/pages/income/income_form_page.dart
+++ lib/app/pages/income/income_form_page.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:myfinance/app/core/config/app_colors.dart';
+import 'package:myfinance/app/core/widgets/text_field_widget.dart';
 
 class IncomeFormPage extends StatelessWidget{
   IncomeFormPage({Key? key}) : super(key: key);
 
   final _formKey = GlobalKey<FormState>();
+  final incomeTextController = TextEditingController();
 
 
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
-      body: Text("Income Form Page"),
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          width: size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(29.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      TextFieldWidget(
+                        controller: incomeTextController,
+                        icon: Icons.money, 
+                        label: "Ganho")
+                    ],
+                  )
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
