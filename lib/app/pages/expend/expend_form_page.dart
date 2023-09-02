c982d3ccaa2099fd74bf931c323f3feeb140d67b
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-09-02 00:01:30 -0300

    definição da estrutura da page de cadastro de gastos

---
+++ lib/app/pages/expend/expend_form_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ExpendFormPage extends StatelessWidget{
+  ExpendFormPage({Key? key}) : super(key: key);
+
+  final _formKey = GlobalKey<FormState>();
+
+
+  @override
+  Widget build(BuildContext context){
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+
+    )
+  }
+}
