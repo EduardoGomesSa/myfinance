c3b567996de925f16d5c01f700f33fa06aee5f2f
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-09-02 00:23:32 -0300

    adição do campo de nome da conta

--- lib/app/pages/base_page.dart
+++ lib/app/pages/base_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myfinance/app/core/config/app_colors.dart';
+import 'package:myfinance/app/pages/bill/bill_form_page.dart';
 import 'package:myfinance/app/pages/home/home_page.dart';
 import 'package:myfinance/app/pages/income/income_form_page.dart';
 import 'package:myfinance/app/pages/profile/profile_page.dart';
@@ -25,6 +26,7 @@ class _BasePageState extends State<BasePage>{
           children: [
             const HomePage(),
             IncomeFormPage(),
+            BillFormPage(),
             const ProfilePage(),
           ],
         ),
@@ -47,6 +49,9 @@ class _BasePageState extends State<BasePage>{
             BottomNavigationBarItem(
               icon: Icon(Icons.add_card_outlined),
               label: "Add Income"),
+             BottomNavigationBarItem(
+              icon: Icon(Icons.add_card_outlined),
+              label: "Add Bill"),
             BottomNavigationBarItem(
               icon: Icon(Icons.people_alt_outlined),
               label: "Profile"),
