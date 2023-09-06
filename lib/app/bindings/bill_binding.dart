fe60b6bd9b15e5529108200c8ca0a194b2c92b84
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-09-06 10:30:35 -0300

    criação da logica do billbinding e criação do arquivo extendbinding

---
+++ lib/app/bindings/bill_binding.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:myfinance/app/controllers/bill_controller.dart';
+import 'package:myfinance/app/repositories/bill_repository.dart';
+
+class BillBinding extends Bindings{
+  @override
+  void dependencies(){
+    Get.put(BillRepository(httpManager: Get.find(), appUtils: Get.find()));
+    Get.put(BillController(auth: Get.find(), repository: Get.find(), appUtils: Get.find()));
+  }
+}
