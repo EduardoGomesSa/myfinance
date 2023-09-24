7b8cf5d8721d6498b7baf8d251509ce41af2badc
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2023-09-23 22:26:15 -0300

    remoção do tipo no construtor da IncomeInfoPage

--- lib/app/pages/income/income_info_page.dart
+++ lib/app/pages/income/income_info_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:myfinance/app/models/income_model.dart';
 
 class IncomeInfoPage extends StatelessWidget{
-  IncomeInfoPage({Key? key, required IncomeModel this.income}) : super(key: key);
+  IncomeInfoPage({Key? key, required this.income}) : super(key: key);
 
   final IncomeModel income;
 
@@ -10,8 +10,8 @@ class IncomeInfoPage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return const Scaffold(
-      body: Text("Tela com informações sobre os Incomes"),
+    return Scaffold(
+      body: Text("Tela com informações sobre os Incomes ${income.value}"),
     );
   }
 }
