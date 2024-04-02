47708cd2c718508f6f4b4850ba6ca443aa62c2d3
EDUARDO GOMES DE SA <99502227+EduardoGomesSa@users.noreply.github.com>
2024-04-01 23:56:10 -0300

    Linha com mês e ano agrupando os dados

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -4,14 +4,15 @@ import 'package:myfinance/app/controllers/income_controller.dart';
 import 'package:myfinance/app/core/widgets/income_widget.dart';
 import 'package:myfinance/app/models/income_model.dart';
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatelessWidget {
   HomePage({super.key});
 
-  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      GlobalKey<RefreshIndicatorState>();
   final IncomeController controller = Get.find();
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
@@ -23,18 +24,89 @@ class HomePage extends StatelessWidget{
         },
         child: GetX<IncomeController>(
           init: controller,
-          builder: (controller){
-            if(controller.isLoading.value == true){
+          builder: (controller) {
+            if (controller.isLoading.value == true) {
               return const Center(child: CircularProgressIndicator());
             } else {
-              return ListView.builder(
-                physics: const BouncingScrollPhysics(),
-                itemCount: controller.listIncome.length,
-                itemBuilder: (BuildContext context, int index){
-                  IncomeModel model = controller.listIncome[index];
+              List<IncomeModel> sortedList =
+                  List.from(controller.listIncome);
+              sortedList.sort((a, b) => a.created!.compareTo(b.created!));
 
-                  return IncomeWidget(model: model);
-                });
+              List<Widget> widgets = [];
+
+              // Variável para armazenar o mês e ano do modelo anterior
+              String previousMonthAndYear = '';
+
+              // Percorre os modelos classificados
+              for (int index = 0; index < sortedList.length; index++) {
+                IncomeModel model = sortedList[index];
+
+                // Obtém o mês e ano do modelo atual
+                String currentMonthAndYear =
+                    '${model.created?.month} - ${model.created?.year}';
+
+                // Se a data do modelo atual for diferente da anterior,
+                // adiciona uma linha de separação
+                if (currentMonthAndYear != previousMonthAndYear) {
+                  // Adiciona a linha com o mês e ano
+                  widgets.add(
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              height: 1,
+                              decoration: const BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: Colors.black, // Cor da linha
+                                    width: 2, // Largura da linha
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text(
+                              currentMonthAndYear,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              height: 1,
+                              decoration: const BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                    color: Colors.black, // Cor da linha
+                                    width: 2, // Largura da linha
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+
+                  // Atualiza o mês e ano anterior para o atual
+                  previousMonthAndYear = currentMonthAndYear;
+                }
+
+                // Adiciona o widget do modelo atual
+                widgets.add(IncomeWidget(model: model));
+              }
+
+              return ListView(
+                physics: const BouncingScrollPhysics(),
+                children: widgets,
+              );
             }
           },
         ),
